9af24248cc2b3a44209dd9e0608f091e06a97b37
Andrei-Bogdan Tomoiagă <andreitom@yahoo.com>
2020-10-14 20:33:37 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tomoiaga_Andrei_Bogdan_Lab2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

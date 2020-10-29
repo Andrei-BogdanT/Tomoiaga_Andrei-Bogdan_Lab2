5fbd6fe7ec09a15aa14af8e8aacd5b7c027abf52
Andrei-Bogdan Tomoiagă <andreitom@yahoo.com>
2020-10-29 21:19:29 +0200

    Am facut toate cerintele din Laboratorul 4.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,16 +6,32 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Tomoiaga_Andrei_Bogdan_Lab2.Models;
+using Microsoft.EntityFrameworkCore;
+using Tomoiaga_Andrei_Bogdan_Lab2.Data;
+using Tomoiaga_Andrei_Bogdan_Lab2.Models.LibraryViewModels;
 
 namespace Tomoiaga_Andrei_Bogdan_Lab2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly LibraryContext _context;
+        public HomeController(LibraryContext context)
         {
-            _logger = logger;
+            _context = context;
+        }
+
+        public async Task<ActionResult> Statistics()
+        {
+            IQueryable<OrderGroup> data =
+            from order in _context.Orders
+            group order by order.OrderDate into dateGroup
+            select new OrderGroup()
+            {
+                OrderDate = dateGroup.Key,
+                BookCount = dateGroup.Count()
+            };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Index()
--- Models/PublishedBook.cs
+++ Models/PublishedBook.cs
@@ -5,15 +5,11 @@ using System.Threading.Tasks;
 
 namespace Tomoiaga_Andrei_Bogdan_Lab2.Models
 {
-    public class Order
+    public class PublishedBook
     {
-        public int OrderID { get; set; }
-        public int CustomerID { get; set; }
+        public int PublisherID { get; set; }
         public int BookID { get; set; }
-
-        public DateTime OrderDate { get; set; }
-
-        public Customer Customer { get; set; }
+        public Publisher Publisher { get; set; }
         public Book Book { get; set; }
     }
 }

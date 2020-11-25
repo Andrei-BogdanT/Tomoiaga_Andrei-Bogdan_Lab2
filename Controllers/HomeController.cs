2361cababd0a6f8f7ba1e8116674e896830997dc
Andrei-Bogdan Tomoiagă <andreitom@yahoo.com>
2020-11-25 20:02:25 +0200

    Am facut toate modificarile din laboratorul 8.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -49,5 +49,10 @@ namespace Tomoiaga_Andrei_Bogdan_Lab2.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Chat()
+        {
+            return View();
+        }
     }
 }
